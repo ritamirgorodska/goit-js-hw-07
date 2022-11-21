5c46758e91e849cbe7138673d8df2f9ea7ab614f
ritamirgorodska <ritamirgorodska.1604@gmail.com>
2022-11-21 12:48:18 +0300

    edit files

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const listGallery = document.querySelector(".gallery");
+const markUp = createGalery(galleryItems);
+listGallery.innerHTML = markUp;
+function createGalery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+      </div>`;
+    })
+    .join("");
+}
+const imgs = document.querySelector(".gallery__image");
+listGallery.addEventListener("click", showModal);
+function showModal(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const example = basicLightbox.create(
+    `
+        <img src="${evt.target.dataset.source}" alt="${evt.target.alt}">
+    `
+  );
 
-console.log(galleryItems);
+  example.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const listGallery = document.querySelector(".gallery");
+const markUp = doGallery(galleryItems);
+listGallery.innerHTML = markUp;
+function doGallery(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/>
+      </a>`;
+    })
+    .join("");
+}
+
+const lightBox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: `alt`,
+});
